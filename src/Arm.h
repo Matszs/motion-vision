d4d4534556b3ee8605a452f20a9ac4a7ff980dcc
Mats Otten <mats@kepsi.nl>
2017-09-12 11:45:49 +0200

    Classes

---
+++ src/Arm.h
@@ -0,0 +1,14 @@
+//
+// Created by Mats Otten on 12/09/2017.
+//
+
+#ifndef MOTION_VISION_ARM_H
+#define MOTION_VISION_ARM_H
+
+
+class Arm {
+
+};
+
+
+#endif //MOTION_VISION_ARM_H
---
+++ src/Robot.h
@@ -0,0 +1,14 @@
+//
+// Created by Mats Otten on 12/09/2017.
+//
+
+#ifndef MOTION_VISION_ROBOT_H
+#define MOTION_VISION_ROBOT_H
+
+
+class Robot {
+
+};
+
+
+#endif //MOTION_VISION_ROBOT_H
